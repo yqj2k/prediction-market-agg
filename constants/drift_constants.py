dfc6bf5ff5f70509bb6d0ebdfa251a62cff408be
pocarisweatdo <mantine08@gmail.com>
2024-09-21 23:25:31 -0700

    updated drift constants

--- constants/drift_constants.py
+++ constants/drift_constants.py
@@ -1,1 +1,15 @@
 MAX_PRICE = 1000000
+HARDCODED_MARKETS_MAP = {
+    "TRUMP-WIN-2024-BET": "Will Donald Trump win the 2024 Presidential Election?",
+    "KAMALA-POPULAR-VOTE-2024-BET": "Will Kamala Harris win the popular vote in 2024?",
+    "REPUBLICAN-POPULAR-AND-WIN-BET": "Will the Republican Party win the election and popular vote?",
+    "BREAKPOINT-IGGYERIC-BET": "Will Iggy Azalea win the celebcoin debate at Solana Breakpoint 2024?",
+    "DEMOCRATS-WIN-MICHIGAN-BET": "Will the Democrats win in Michigan?",
+}
+HARDCODED_MARKETS_NAMES = [
+    "TRUMP-WIN-2024-BET",
+    "KAMALA-POPULAR-VOTE-2024-BET",
+    "REPUBLICAN-POPULAR-AND-WIN-BET",
+    "BREAKPOINT-IGGYERIC-BET",
+    "DEMOCRATS-WIN-MICHIGAN-BET",
+]
--- scrapers/drift_scraper.py
+++ scrapers/drift_scraper.py
@@ -1,23 +1,8 @@
 import requests
 
-from constants.drift_constants import MAX_PRICE
+from constants.drift_constants import HARDCODED_MARKETS_MAP, HARDCODED_MARKETS_NAMES, MAX_PRICE
 import websocket_handler
 from websocket_processors.drift_ws_processor import DriftSubscriptionMessage, DriftWSProcessor
-
-HARDCODED_MARKETS_MAP = {
-    "TRUMP-WIN-2024-BET": "Will Donald Trump win the 2024 Presidential Election?",
-    "KAMALA-POPULAR-VOTE-2024-BET": "Will Kamala Harris win the popular vote in 2024?",
-    "REPUBLICAN-POPULAR-AND-WIN-BET": "Will the Republican Party win the election and popular vote?",
-    "BREAKPOINT-IGGYERIC-BET": "Will Iggy Azalea win the celebcoin debate at Solana Breakpoint 2024?",
-    "DEMOCRATS-WIN-MICHIGAN-BET": "Will the Democrats win in Michigan?",
-}
-HARDCODED_MARKETS_NAMES = [
-    "TRUMP-WIN-2024-BET",
-    "KAMALA-POPULAR-VOTE-2024-BET",
-    "REPUBLICAN-POPULAR-AND-WIN-BET",
-    "BREAKPOINT-IGGYERIC-BET",
-    "DEMOCRATS-WIN-MICHIGAN-BET",
-]
 
 GET = "GET"
 POST = "POST"
