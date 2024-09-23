8324a6ab07fc90709359c2360dc420d499174eb1
samue <samuelip234@gmail.com>
2024-09-22 23:51:29 -0700

    Add limitless scraper

---
+++ scrapers/limitless_scraper.py
@@ -0,0 +1,98 @@
+import requests
+import json
+
+import websocket_handler
+from websocket_processors.limitless_ws_processor import (
+    LimitlessWSProcessor
+)
+
+
+GET = "GET"
+POST = "POST"
+DELETE = "DELETE"
+PUT = "PUT"
+
+HOST = "https://api.limitless.exchange/markets/active"
+WS_HOST = "wss://ws-subscriptions-clob.polymarket.com/ws/market"
+COLLECTION_NAME = "limitless_events"
+
+# for betting URL, limitless.exchange/markets/{address} 
+
+class Market:
+    def __init__(self, market):
+        # print("individual market: " + str(market))
+        self.address = market["address"]
+        self.title = market["title"]
+        self.created_date = market["createdAt"]
+        if "deadline" in market:
+            self.end_date = market["deadline"]
+        else:
+            print("This market is missing a deadline: " + market["address"])
+            self.end_date = "2025-12-31T12:00:00Z"
+        self.liquidity = market["liquidityFormatted"]
+        self.volume = market["volumeFormatted"]
+        self.collateralAsset = market["collateralToken"]["symbol"]
+        self.outcomes = ["Yes", "No"]
+        self.prices = None
+
+
+    def __repr__(self):
+        return f"Market address:{self.address}, title: {self.title}, createdAt: {self.created_date}, endDate: {self.end_date}, liquidity: {self.liquidity}, volume: {self.volume} \n"
+
+
+def init_limitless(mongodb_client):
+    resp = requests.request(GET, HOST)
+    if resp.status_code != 200:
+        print("Request to limitless API erroring out, stopping execution")
+        return
+
+    markets = resp.json()
+
+    list_markets = []
+    for market in markets:
+        print(market)
+        m_event = market['markets'] if 'markets' in market else []
+
+        if len(m_event) > 1:
+            print(
+                "This is an event with multiple outcomes, skipping for now: {} + id: {}",
+                market["title"]
+            )
+            continue
+
+        new_market = Market(market)
+        list_markets.append(new_market)
+
+        # find if document exists in collection, otherwise push it
+        query = {"address": new_market.address}
+        existing_market = mongodb_client.read(COLLECTION_NAME, query)
+
+        if existing_market is None:
+            res = mongodb_client.create(COLLECTION_NAME, new_market.__dict__)
+
+            # Store each TokenID as K-V (Token - Market Id)
+            # mongodb_poly_kv_store_client.set(new_market.tokenIds[0], new_market._id)
+            # mongodb_poly_kv_store_client.set(new_market.tokenIds[1], new_market._id)
+
+            print("Inserted document id: " + str(res))
+        else:
+            print("Market exists already, updates only happen during WS/feed connection")
+
+
+# async def init_limitless_ws(mongodb_client, arbitrage_handler):
+#     list_markets = mongodb_client.read_all(COLLECTION_NAME)
+
+#     all_addresses = [market["tokenIds"] for market in list_markets]
+#     flattened_token_ids = [
+#         token_id for sublist in all_token_ids for token_id in sublist
+#     ]
+
+#     poly_ws_processor = PolyWSProcessor(
+#         [PolySubscriptionMessage({}, [], flattened_token_ids, "Market")],
+#         COLLECTION_NAME,
+#         mongodb_client,
+#         mongodb_poly_kv_store_client,
+#         arbitrage_handler,
+#     )
+
+#     await websocket_handler.open_ws_connection(WS_HOST, poly_ws_processor)
---
+++ websocket_processors/limitless_ws_processor.py
@@ -0,0 +1,75 @@
+from websocket_processors.ws_processor import WSProcessor
+import logging
+import json
+
+class PriceChangeEvent:
+    def __init__(self, event_type, market, timestamp, address, outcome, contracts, amount, amountUSD, symbol, strategy):
+        self.event_type = event_type
+        self.market = market
+        self.timestamp = timestamp
+        self.address = address
+        self.outcome = outcome
+        self.contracts = contracts
+        self.amount = amount
+        self.amountUSD = amountUSD
+        self.symbol = symbol
+        self.strategy = strategy
+
+def handle_price_change(event):
+    # logging.info(event)
+    price_change_event = PriceChangeEvent(
+        event["eventType"],
+        event["title"],
+        event["data"]["timestamp"],
+        event["data"]["address"],
+        event["data"]["outcome"],
+        event["data"]["contracts"],
+        event["data"]["tradeAmount"],
+        event["data"]["tradeAmountUSD"],
+        event["data"]["symbol"],
+        event["data"]["strategy"]
+    )
+    # logging.info("Price change detected: assetID: %s, New Price: %s, Time: %s",
+    #              price_change_event.asset_id, price_change_event.price, price_change_event.timestamp)
+    return price_change_event
+
+class LimitlessWSProcessor(WSProcessor):
+    def __init__(
+        self,
+        subscription_message,
+        collection_name,
+        db_client,
+        kv_client,
+        arbitrage_handler,
+    ):
+        self.subscription_message = subscription_message
+        self.db_client = db_client
+        self.kv_client = kv_client
+        self.collection_name = collection_name
+        self.arbitrage_handler = arbitrage_handler
+
+    def createSubcriptionMessages(self):
+        return self.subscription_message
+
+    async def processMessage(self, message):
+        event = json.loads(message)
+        event_type = event.get("event_type")
+
+        if event_type == "NEW_TRADE":
+            price_change_event = handle_price_change(event)
+            address = price_change_event.address
+
+            # market_id = self.kv_client.get(address)
+            market = self.db_client.read(self.collection_name, {"_address": address})
+            
+            if not (address is None and market is None):
+                index = 0 if market["outcome"] == "YES" else 1
+
+                if not market["prices"][index] == price_change_event.price:
+                    market["prices"][index] = price_change_event.price
+                    new_prices = market["prices"]
+                    self.db_client.update(
+                        self.collection_name, {"_address": address}, {"prices": new_prices}
+                    )
+                    self.arbitrage_handler.handle("limitless", market)
+                    # logging.info("Price change detected: assetID: %s, New Price: %s, Time: %s", price_change_event.asset_id, price_change_event.price, price_change_event.timestamp)
